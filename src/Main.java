cf4ac4bf87e81440e56f7a0f87fdfa9e16e52f31
ErickSilva-Debug <ericksilvad8@gmail.comn>
2024-12-23 15:31:59 -0300

    primeiro comando em Java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+/* todo arquivo Java se forem escrever, vão precisar declarar uma classe, tem que ser nome do arquivo */
+/*so posso declarar somente uma classe para cada arquivo? não pode criar uma classe, mas tem que existir classe contem nome do arquivo*/
+// public tem que usar quando nome da classe e o arquivo
+//classe em Java é um bloco onde você define as características do objeto
+//método é uma função que pertence a uma classe ou objeto, então crio objeto tem nome helloworld, classe eu vou colocar as características nelas e método e a função que o código ira fazer .
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
